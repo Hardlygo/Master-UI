6e013b08b287f335ad789a3fa1f8fe3c392c71c9
Hardlygo <961332109@qq.com>
2020-05-31 21:24:26 +0800

    增加css全局变量，以及cell-group组件

---
+++ src/components/m-cell-group/index.js
@@ -0,0 +1,44 @@
+/*
+ * @Author: your name
+ * @Date: 2020-05-31 13:23:40
+ * @LastEditTime: 2020-05-31 19:28:45
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \blog_vuee:\JSWorkSpace\master-ui\src\components\m-cell-group\index.js
+ */
+import "./index.styl"
+export default {
+  name: "MCellGorup",
+  props: {
+    title: String,
+    border: Boolean,
+  },
+  methods: {},
+  render(h) {
+    const slots = this.$slots;
+    const groupProps = {
+      class: ["m-cell-group", this. border ? "m-cell-group--border" : ""], //使用bem命名
+      attrs: {
+        ...this.$attrs,
+      },
+      on: {
+        ...this.$listeners,
+      },
+    };
+    let groupTitle = null;
+    if (this.title || slots.title) {
+      groupTitle = (
+        <div {...{ class: ["m-cell-group_title"] }}>
+          {slots.title ? slots.title : this.title}
+        </div>
+      );
+    }
+
+    return (
+      <div {...groupProps}>
+        {groupTitle}
+        <div {...{ class: ["m-cell-group_body"] }}>{slots.default || null}</div>
+      </div>
+    );
+  },
+};
--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,36 +1,40 @@
 <!--
  * @Author: your name
  * @Date: 2020-05-30 16:04:40
- * @LastEditTime: 2020-05-30 22:35:17
+ * @LastEditTime: 2020-05-31 19:29:17
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \blog_vuee:\JSWorkSpace\master-ui\src\views\Test.vue
 --> 
 <template>
   <div>
-    <m-input
-      type="number"
-      label="文本"
-      placeholder="请输入文本"
-      clearable
-      v-model="myVal"
-    >
-      <!-- <template v-slot:right-icon> -->
-      <!-- <v-icon>mdi-close</v-icon> -->
-      <!-- <m-icon name="delete" /> -->
-      <!-- </template> -->
-      <!-- <template #button>
+    <m-cell-group border title="输入框">
+      <m-input
+        type="number"
+        label="文本"
+        placeholder="请输入文本"
+        clearable
+        v-model="myVal"
+      >
+        <!-- <template v-slot:right-icon> -->
+        <!-- <v-icon>mdi-close</v-icon> -->
+        <!-- <m-icon name="delete" /> -->
+        <!-- </template> -->
+        <!-- <template #button>
         <v-btn text>发送短信</v-btn>
       </template> -->
-    </m-input>
+      </m-input>
+    </m-cell-group>
     {{myVal}}
   </div>
 </template>
 <script>
 import MInput from "@/components/m-input";
+import MCellGroup from "@/components/m-cell-group";
 export default {
   components: {
-    MInput
+    MInput,
+    MCellGroup
   },
   data() {
     return {
