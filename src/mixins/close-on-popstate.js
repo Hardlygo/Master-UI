ed2053cb45924b5d690db68e5b6f7014813121ff
Hardlygo <961332109@qq.com>
2020-07-01 23:14:05 +0800

    完善popstate

--- src/mixins/close-on-popstate.js
+++ src/mixins/close-on-popstate.js
@@ -1,2 +1,40 @@
-import {on,off} from '../utils/dom/event'
-import {BindEventMixin} from './bind-evnet'
+import { on, off } from "../utils/dom/event";
+import { BindEventMixin } from "./bind-evnet";
+
+export const CloseOnPopstateMixin = {
+  mixins: [
+    BindEventMixin(function(bind, isBind) {
+      this.handlePopstate(isBind && this.closeOnPopstate);
+    }),
+  ],
+  props: {
+    closeOnPopstate: [Boolean],
+  },
+  data() {
+    return {
+      bindStatus: false,
+    };
+  },
+  watch: {
+    closeOnPopstate(val) {
+      this.handlePopstate(val);
+    },
+  },
+  methods: {
+    handlePopstate(bind) {
+      /* istanbul ignore if */
+      if (this.$isServer) {
+        return;
+      }
+
+      if (this.bindStatus !== bind) {
+        this.bindStatus = bind;
+        const action = bind ? on : off;
+        action(window, "popstate", () => {
+          this.close();
+          this.shouldReopen = false;
+        });
+      }
+    },
+  },
+};
