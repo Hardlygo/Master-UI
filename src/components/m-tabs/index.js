2298b70ef892fe98bfb65342e227f188c746bef7
pengzy <961332109@qq.com>
2020-07-30 21:21:27 +0800

    编写tabs中

--- src/components/m-tabs/index.js
+++ src/components/m-tabs/index.js
@@ -2,15 +2,368 @@
 import { ParentMixin } from "../../mixins/relation";
 //提供取得slot的方法=》this.slots
 import { SlotsMixin } from "../../mixins/slots";
+import { BindEventMixin } from "../../mixins/bind-evnet";
 
+//utils
+import { createNameSpace, isDef, isPromise } from "../../utils";
+import { addUnit } from "../../utils/format/unit";
+import { scrollLeftTo, scrollTopTo } from "./utils";
+import { route } from "../../utils/router";
+import { isHidden } from "../../utils/dom/style";
+import { on, off } from "../../utils/dom/event";
+import {
+  getElementTop,
+  getScroller,
+  getVisibleTop,
+  setRootScrollTop,
+  getVisibleHeight,
+} from "../../utils/dom/scroll";
 //bem
-import { createNameSpace, isDef } from "../../utils";
 const { bem } = createNameSpace("tabs");
-
+const BORDER_TOP_BOTTOM = "m-hairline--top-bottom";
+//component
+import Sticky from "../sticky";
+import Title from "./Title";
+import Content from "./Content";
 export default {
   name: "MTabs",
-  mixins: [ParentMixin("MTabs"),SlotsMixin],
-  props:{
-      
-  }
+  mixins: [
+    ParentMixin("MTabs"),
+    BindEventMixin(function(bind) {
+      if (!this.scroller) {
+        this.scroller = getScroller(this.$el);
+      }
+      bind(window, "resize", this.resize, true);
+      if (this.scrollspy) {
+        bind(this.scroller, "scroll", this.onScroll, true);
+      }
+    }),
+    SlotsMixin,
+  ],
+  props: {
+    color: String,
+    sticky: Boolean,
+    animated: Boolean,
+    swipeable: Boolean,
+    scrollspy: Boolean,
+    background: String,
+    lineWidth: [Number, String],
+    lineHeight: [Number, String],
+    beforeChange: Function,
+    titleActiveColor: String,
+    titleOffColor: String,
+    type: {
+      type: String,
+      default: "line",
+    },
+    value: {
+      type: [Number, String],
+      default: 0,
+    },
+    border: {
+      type: Boolean,
+      default: true,
+    },
+    //省略过长title文字
+    ellipsis: {
+      type: Boolean,
+      default: true,
+    },
+    duration: {
+      type: [Number, String],
+      default: 0.3,
+    },
+    offsetTop: {
+      type: [Number, String],
+      default: 0,
+    },
+    lazyRender: {
+      type: Boolean,
+      default: true,
+    },
+    swipeThreshold: {
+      type: [Number, String],
+      default: 4,
+    },
+  },
+  data() {
+    return {
+      position: "",
+      currentIndex: null,
+      lineStyle: {
+        backgroundColor: this.color,
+      },
+    };
+  },
+  computed: {
+    //是否可滑动
+    scrollable() {
+      return this.childern.length > this.swipeThreshold || !this.ellipsis;
+    },
+    navStyle() {
+      return {
+        borderColor: this.color,
+        background: this.background,
+      };
+    },
+    currentVal() {
+      const activeTab = this.childern[this.currentIndex];
+      if (activeTab) {
+        return activeTab.computedVal;
+      }
+    },
+    scrollOffset() {
+      if (this.sticky) {
+        return +this.offsetTop + this.tabHeight;
+      }
+      return 0;
+    },
+  },
+  methods: {
+    resize() {
+      this.setLine();
+    },
+    init() {
+      this.$nextTrick(() => {
+        this.inited = true;
+        this.tabHeight = getVisibleHeight(this.$refs.wrap);
+        this.scrollIntoView(true);
+      });
+    },
+    // update nav bar
+    setLine() {
+      const shouldAnimate = this.inited;
+      this.$nextTrick(() => {
+        const { titles } = this.$refs;
+        if (
+          !titles ||
+          !titles[this.currentIndex] ||
+          this.type !== "line" ||
+          isHidden(this.$el)
+        ) {
+          return;
+        }
+
+        const title = titles[this.currentIndex].$el;
+        const { lineHeight, lineWidth } = this;
+        const width = isDef(lineWidth) ? lineWidth : title.offsetWidth / 2;
+        const left = title.offsetLeft + title.offsetWidth / 2;
+
+        const lineStyle = {
+          width: addUnit(width),
+          backgroundColor: this.color,
+          transform: `translateX(${left}px) translateX(-50%)`,
+        };
+        if (shouldAnimate) {
+          lineStyle.transitionDuration = `${this.duration}s`;
+        }
+        if (isDef(lineHeight)) {
+          const height = addUnit(lineHeight);
+          lineStyle.height = height;
+          lineStyle.borderRadius = height;
+        }
+        this.lineStyle = lineStyle;
+      });
+    },
+    // correct the index of active tab
+    setCurrentIndexByVal(value) {
+      const matched = this.childern.filter((tab) => tab.computedVal === value);
+      const defaultIndex = (this.childern[0] || {}).index || 0;
+      this.setCurrentIndex(matched.length ? matched[0].index : defaultIndex);
+    },
+    setCurrentIndex(currentIndex) {
+      currentIndex = this.findAvailableTab(currentIndex);
+      if (isDef(currentIndex) && currentIndex !== this.currentIndex) {
+        const shouldEmitChange = this.currentIndex != null;
+        this.currentIndex = currentIndex;
+        this.$emit("input", this.currentVal);
+
+        if (shouldEmitChange) {
+          this.$emit(
+            "change",
+            this.currentVal,
+            this.children[currentIndex].title
+          );
+        }
+      }
+    },
+    //确定index是往前减还是往后加
+    findAvailableTab(index) {
+      const diff = index < this.currentIndex ? -1 : 1;
+
+      while (index >= 0 && index < this.children.length) {
+        if (!this.children[index].disabled) {
+          return index;
+        }
+
+        index += diff;
+      }
+    },
+    callBeforeChange(value, done) {
+      if (this.beforeChange) {
+        const returnVal = this.beforeChange(value);
+
+        if (isPromise(returnVal)) {
+          returnVal.then((value) => {
+            if (value) {
+              done();
+            }
+          });
+        } else if (returnVal) {
+          done();
+        }
+      } else {
+        done();
+      }
+    },
+    // emit event when clicked
+    onClick(component, index) {
+      const { title, disabled, computedVal } = this.childern[index];
+      if (disabled) {
+        this.$emit("disabled", computedVal, title);
+      } else {
+        this.callBeforeChange(computedVal, () => {
+          this.setCurrentIndex(index), this.scrollToCurrentContent();
+        });
+        this.$emit("click", computedVal, title);
+        route(item.$router, component);
+      }
+    },
+
+    scrollIntoView(immediate) {
+      const { titles } = this.$refs;
+      if (!this.scrollable || !this.titles || !this.titles[this.currentIndex]) {
+        return;
+      }
+      const { nav } = this.$refs;
+
+      const title = titles[this.currentIndex].$el;
+      const to = title.offsetLeft - (nav.offsetWidth - title.offsetWidth) / 2;
+      scrollLeftTo(nav, to, immediate ? 0 : this.duration);
+    },
+
+    onStickyScroll(params) {
+      this.stickyFixed = params.isFixed;
+      this.$emit("scroll", params);
+    },
+    scrollTo(val) {
+      this.$nextTick(() => {
+        this.setCurrentIndexByVal(val);
+        this.scrollToCurrentContent(true);
+      });
+    },
+    scrollToCurrentContent(immediate = false) {
+      if (this.scrollspy) {
+        const target = this.children[this.currentIndex];
+        const el = target?.$el;
+
+        if (el) {
+          const to = getElementTop(el, this.scroller) - this.scrollOffset;
+
+          this.lockScroll = true;
+          scrollTopTo(this.scroller, to, immediate ? 0 : +this.duration, () => {
+            this.lockScroll = false;
+          });
+        }
+      }
+    },
+    onScroll() {
+      if (this.scrollspy && !this.lockScroll) {
+        const index = this.getCurrentIndexOnScroll();
+        this.setCurrentIndex(index);
+      }
+    },
+    getCurrentIndexOnScroll() {
+      const { children } = this;
+
+      for (let index = 0; index < children.length; index++) {
+        const top = getVisibleTop(children[index].$el);
+
+        if (top > this.scrollOffset) {
+          return index === 0 ? 0 : index - 1;
+        }
+      }
+
+      return children.length - 1;
+    },
+  },
+  render() {
+    const { type, ellipsis, animated, scrollable } = this;
+
+    const Nav = this.children.map((item, index) => (
+      <Title
+        ref="titles"
+        refInFor
+        type={type}
+        dot={item.dot}
+        info={isDef(item.badge) ? item.badge : item.info}
+        title={item.title}
+        color={this.color}
+        style={item.titleStyle}
+        isActive={index === this.currentIndex}
+        ellipsis={ellipsis}
+        disabled={item.disabled}
+        scrollable={scrollable}
+        activeColor={this.titleActiveColor}
+        inactiveColor={this.titleInactiveColor}
+        swipeThreshold={this.swipeThreshold}
+        scopedSlots={{
+          default: () => item.slots("title"),
+        }}
+        onClick={() => {
+          this.onClick(item, index);
+        }}
+      />
+    ));
+
+    const Wrap = (
+      <div
+        ref="wrap"
+        class={[
+          bem("wrap", { scrollable }),
+          { [BORDER_TOP_BOTTOM]: type === "line" && this.border },
+        ]}
+      >
+        <div
+          ref="nav"
+          role="tablist"
+          class={bem("nav", [type])}
+          style={this.navStyle}
+        >
+          {this.slots("nav-left")}
+          {Nav}
+          {type === "line" && (
+            <div class={bem("line")} style={this.lineStyle} />
+          )}
+          {this.slots("nav-right")}
+        </div>
+      </div>
+    );
+
+    return (
+      <div class={bem([type])}>
+        {this.sticky ? (
+          <Sticky
+            container={this.$el}
+            offsetTop={this.offsetTop}
+            onScroll={this.onSticktScroll}
+          >
+            {Wrap}
+          </Sticky>
+        ) : (
+          Wrap
+        )}
+        <Content
+          count={this.children.length}
+          animated={animated}
+          duration={this.duration}
+          swipeable={this.swipeable}
+          currentIndex={this.currentIndex}
+          onChange={this.setCurrentIndex}
+        >
+          {this.slots()}
+        </Content>
+      </div>
+    );
+  },
 };
--- src/components/sticky/index.js
+++ src/components/sticky/index.js
@@ -1,5 +1,142 @@
 import "./index.styl";
 
+//utils
+import { isHidden } from "../../utils/dom/style";
+import { unit2px } from "../../utils/format/unit";
+import {
+  getElementTop,
+  getScrollTop,
+  getScroller,
+} from "../../utils/dom/scroll";
+import { isDef, isServer, createNameSpace } from "../../utils";
+const { bem } = createNameSpace("sticky");
+
+//mixins
+import { BindEventMixin } from "../../mixins/bind-evnet";
+//提供取得slot的方法=》this.slots
+import { SlotsMixin } from "../../mixins/slots";
+
 export default {
   name: "Sticky",
+  mixins: [
+    BindEventMixin(function(bindMethod, isBind) {
+      if (!this.scroller) {
+        this.scroller = getScroller(this.$el);
+      }
+      if (this.observer) {
+        const method = isBind ? "observe" : "unobserve";
+        this.observer[method](this.$el);
+      }
+      //调用BindEventMixin绑定的bind监听方法，见BindEventMixin的on/off
+      bindMethod(this.scroller, "scroll", this.onScroll, true);
+      this.onScroll();
+    }),
+    SlotsMixin,
+  ],
+  props: {
+    zIndex: [String, Number],
+    container: null,
+    offsetTop: {
+      type: [String, Number],
+      default: 0,
+    },
+  },
+  data() {
+    return {
+      fixed: false,
+      height: 0,
+      transform: 0,
+    };
+  },
+  computed: {
+    offsetTopPx() {
+      return unit2px(this.offsetTop);
+    },
+    style() {
+      if (!this.fixed) return;
+      const style = {};
+      if (isDef(this.zIndex)) {
+        style.zIndex = this.zIndex;
+      }
+      if (this.offsetTopPx && this.fixed) {
+        style.top = `${this.offsetTopPx}px`;
+      }
+      if (this.transform) {
+        style.transform = `translate3d(0,${this.transform}px,0)`;
+      }
+      return style;
+    },
+  },
+  created() {
+    if (!isServer && window.IntersectionObserver) {
+      //见http://www.ruanyifeng.com/blog/2016/11/intersectionobserver_api.html
+      this.observer = new IntersectionObserver(
+        (entrise) => {
+          //当第一个元素可见时
+          if (entrise[0].intersectionRatio > 0) {
+            this.onScroll();
+          }
+        },
+        //根元素
+        { root: document.body }
+      );
+    }
+  },
+  methods: {
+    onScroll() {
+      if (isHidden(this.$el)) {
+        return;
+      }
+      this.height = this.$el.offsetHight;
+      const { container, offsetTopPx } = this;
+      const scrollTop = getScrollTop(window);
+      const top2pageTop = getElementTop(this.$el);
+
+      const emmitScrollEvent = () => {
+        this.$emit("scroll", {
+          scrollTop,
+          fixed: this.fixed,
+        });
+      };
+
+      if (container) {
+        const bottom2pageTop = top2pageTop + container.offsetHight;
+
+        if (scrollTop + offsetTopPx + this.height > bottom2pageTop) {
+          const distance2bottom = this.height + scrollTop - bottom2pageTop;
+
+          if (distance2bottom < this.height) {
+            this.fixed = true;
+            //上移
+            this.transform = -(distance2bottom + offsetTopPx);
+          } else {
+            this.fixed = false;
+          }
+          emmitScrollEvent();
+          return;
+        }
+      }
+      if (scrollTop + offsetTopPx > top2pageTop) {
+        this.fixed = true;
+        this.transform = 0;
+      } else {
+        this.fixed = false;
+      }
+      emmitScrollEvent();
+    },
+  },
+  render() {
+    const { fixed } = this;
+    const style = {
+      height: fixed ? `${this.height}px` : null,
+    };
+
+    return (
+      <div style={style}>
+        <div class={bem({ fixed })} style={this.style}>
+          {this.slots()}
+        </div>
+      </div>
+    );
+  },
 };
